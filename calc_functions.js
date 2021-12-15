6fe1be47c885fe14ed823b47646f0682d8907ee7
brent <brenton.j.andrews@gmail.com>
2021-12-15 10:26:14 -0700

    Refactor in progress

--- calc_functions.js
+++ calc_functions.js
@@ -1,5 +1,129 @@
-// Calculator functions: add, subtract, divide, multiply and more!
+// Declare buttons, constants.
+let operandA = "";
+let operandB ="";
+let operator = "";
+let operatorSelected = false;
 
+// The variables below store the information presented on the calculator screen.
+let statement_contents = "";
+let output_contents = "";
+let statement_screen = document.getElementById("statement");
+let output_screen = document.getElementById("output");
+
+// Select elements from the DOM.
+let number_buttons = document.querySelectorAll("#num");
+let operator_buttons = document.querySelectorAll("#operator");
+let decimal_button = document.querySelector("#decimal");
+let equals_button = document.getElementById("equals");
+let clear_button = document.getElementById("clear");
+let delete_button = document.getElementById("delete");
+ 
+
+// Add event listeners to each button.
+equals_button.addEventListener("click", () => checkEvaluation());
+clear_button.addEventListener("click", () => clearScreenAndMemory());
+delete_button.addEventListener("click", () => deleteStatementContents());
+decimal_button.addEventListener("click", () => addToStatementContents(decimal_button));
+
+number_buttons.forEach((button => 
+    button.addEventListener("click", () => addToStatementContents(button))
+    )
+)
+
+operator_buttons.forEach((button => 
+    button.addEventListener("click", () => addToStatementContents(button))
+    )
+)
+
+// Add one character to statement_contents. 
+function addToStatementContents(button) {
+    getOpValues(button)
+    if (statement_contents != "") {
+        appendStatementContents(button.textContent);
+    } else {
+        statement_contents = button.textContent;
+    }
+    updateStatementScreen(statement_contents);
+}
+
+// Remove one character from statement_contents.
+function deleteStatementContents() {
+    statement_contents = statement_contents.slice(0, -1);
+    updateStatementScreen();
+}
+
+// Append statement_contents witontentsh additional characters.
+function appendStatementContents(input) {
+    statement_contents += input;
+}
+
+// Transfer value of statement_contents to statement_screen element.
+function updateStatementScreen() {
+    statement_screen.textContent = statement_contents;
+
+}
+
+// Reset variables and update statement and output screens.
+function clearScreenAndMemory()  {
+    operandA = "";
+    operandB ="";
+    operator = "";
+    operatorSelected = false;
+    statement_contents = "";
+    output_screen.textContent = "";
+    updateStatementScreen();
+}
+
+// Add characters to operand values.
+function getOpValues(button) {
+
+    // operator value and operator chaining.
+    if (button.id === "operator") {
+        if (operatorSelected === false) {
+            operator = button.textContent;
+            operatorSelected = true;
+            console.log("Operator: " + operator);
+        }
+        if (operatorSelected === true && operandA != "" && operandB != "") {
+        operandA = output_screen.textContent;
+        operator = button.textContent;
+        statement_contents = operandA;
+        console.log(statement_contents);
+        operandB = "";
+        updateStatementScreen();
+        }
+    }
+
+    // operandB value.
+    if (operatorSelected === true && button.id === "num") {
+        operandB += button.textContent;
+        console.log("Operand B: " + operandB);
+    }
+
+
+
+    // operandA value.
+    if (operatorSelected === false && button.id === "num") {
+        operandA += button.textContent;
+        console.log("Operand A: " + operandA);
+    }
+}
+
+// Check argument operand for more than one decimal. Disable decimal button if so.
+function decimalCheck(button, operand) {
+    if (operand.includes(".")) {
+        return true;
+    }
+}
+
+// Check for valid input before calculations, swap operand values after output.
+function checkEvaluation() {
+    output_screen.textContent = evaluateStatement(operator, Number(operandA), Number(operandB));
+}
+
+
+
+// Calculator functions: add, subtract, divide, multiply and more!
 function add(a, b) {
     return a + b;
 }
@@ -16,7 +140,7 @@ function multiply(a, b) {
     return a * b;
 }
 
-function operate(symbol, a, b) {
+function evaluateStatement(symbol, a, b) {
     let output;
     if (symbol === " + ") {
         output = add(a, b);   
@@ -32,126 +156,5 @@ function operate(symbol, a, b) {
     else if (symbol === " * ") {
         output = multiply(a, b);
     }
-
     return output;
 }
-
-// let screen_contents = document.getElementById("screen");
-// screen_contents.textContent = "Hello"
-
-// Function to add event listeners to each button upon start.
-function addEvents() {
-    let operandA = "";
-    let operandB = "";
-    let operator = "";
-    let operatorSelected = false;
-    let buttons = document.getElementsByClassName("btn");
-    let screen_content = "";
-    let output_content = "";
-    let statement_screen = document.getElementById("statement");
-    let output_screen = document.getElementById("statement_output")
-    for (const button of buttons) {
-        button.addEventListener("click", function() {
-
-            // Clear button event. Reset to starting status.
-            if (button.id == "clear") {
-                operandA = "";
-                operandB = "";
-                operator = "";
-                operatorSelected = false;
-                screen_content = "";
-                output_content = "";
-            }
-
-            // Delete button event. Remove last digit from current statement.
-            if (button.id == "delete") {
-
-                // Delete operandA contents.
-                if (operatorSelected === false) {
-                    operandA = operandA.toString().slice(0, -1);
-                    screen_content = operandA;
-                } 
-                
-                // Delete operandB and / or operator.
-                else if (operatorSelected === true) {
-
-                    // Delete operator.
-                    if (operandB === "") {
-                        operator = "";
-                        operatorSelected = false;
-                        screen_content = operandA;
-                    } 
-                    
-                    // Delete operandB.
-                    else {
-                        operandB = operandB.toString().slice(0, -1);
-                        screen_content = operandA + operator + operandB;
-                    }
-                }
-            }
-
-            // Number button events.
-            if (button.id == "num") {
-
-                // If no operator button pressed, operandA is the one to edit.
-                if (operatorSelected === false) {
-                    if (operandA == "") {
-                        operandA = button.textContent;
-                    } else {
-                        operandA += button.textContent;
-                    }
-
-                } else {
-                    if (operandB == "") {
-                        operandB = button.textContent;
-                    } else {
-                        operandB += button.textContent;
-                    }
-                }
-                screen_content += button.textContent;
-            }
-
-            // Operator button event.
-            if (button.id === "operator") {
-                // Allow for operators to be chained together.
-                if (operandA != "" && operandB != "") {
-                    let output = operate(operator, Number(operandA), Number(operandB));
-                    console.log(output);
-                    if (output != undefined) {
-                        operandA = output;
-                    } else {
-                        operandA = "";
-                    }
-                    operator = button.textContent;
-                    operandB = "";
-                }
-
-                // Prevent operandA from being skipped if an operator button is pressed beforehand.
-                else if (operandA != "") {
-                    operator = button.textContent;
-                    operatorSelected = true;
-                } 
-                screen_content += button.textContent;
-            }
-
-            // Equals button event.
-            if (button.id === "equals" && operator != "" && operandA != "" && operandB != "") {
-                let output = operate(operator, Number(operandA), Number(operandB));
-                if (output != undefined) {
-                    operandA = output;
-                } else {
-                    operandA = "";
-                }
-                operandB = "";
-                operatorSelected = false;
-                output_content = output;
-            }
-
-            statement_screen.textContent = screen_content;
-            output_screen.textContent = output_content;
-        });
-    }
-}
-
-
-addEvents()
